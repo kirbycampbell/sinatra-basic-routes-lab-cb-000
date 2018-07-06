d91971fd67174b30049b7969089a4fbb2a800c34
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-06 07:19:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,18 +13,18 @@ class App < Sinatra::Base
   get '/favorite-song' do
     'My favorite song is "I Love Kanye"'
   end
-  
+
   get '/name' do
     "My name is Kirby"
   end
-  
+
   get '/hometown' do
     "My hometown is Lafayette"
   end
-  
+
   get '/favorite-song' do
     'My favorite song is "I Love Kanye"'
   end
-  
+
 
 end
